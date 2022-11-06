bb48ed2231a018abd7687b5c2d1c25cb2bf1d07f
Jonathan Daniel <gcagrad19@gmail.com>
2022-11-06 17:02:20 -0500

    Added Filtering

--- scripts/model_toolkit.py
+++ scripts/model_toolkit.py
@@ -1,11 +1,51 @@
 import torch
 from gym_wordle.envs.wordle_env import WORDS
+from typing import Tuple, List, Union
+
+
+class InvalidWordProcessorLayer(torch.nn.Module):
+    def __init__(self, words: List[Tuple[int, int, int, int, int]]):
+        super(InvalidWordProcessorLayer, self).__init__()
+        self.words = words
+        self.num_words = len(words)
+        self.unadjusted_letters = set(range(0, 26))
+
+        # Make a set of 26 arrays. Each of them has 0 or 1 multipliers for
+        # each word. Basically, if we know that 'a' cannot be in the word, we take
+        # the tensor for 'a' and multiply that by the WORDS array. This should
+        # strip out all words that don't have that letter. Lots of pre-processing
+        # time potentially, but this makes it run fast in forward() since we're
+        # just doing tensor multiplication.
+
+        # These tensors will have a 1 if the letter doesn't appear in the word.
+        # and a 0 if the letter does appear in the word.
+        self.letter_filters = torch.ones(26, self.num_words)
+
+        for word_num, word in enumerate(words):
+            for letter in word:
+                self.letter_filters[letter][word_num] = 0
+
+    def to(self, device):
+        self.letter_filters = self.letter_filters.to(device)
+        return self
+
+    def forward(self, probs: torch.Tensor, invalid_letters: torch.Tensor) -> torch.Tensor:
+        # At this point, the input should be the softmax probabilities?
+        # If the word contains any invalid letter, we need to remove it.
+        # We can do this by setting the probability of the word to 0.
+        output = probs.clone()
+        for idx, is_invalid in enumerate(invalid_letters):
+            if is_invalid:
+                # Multiply the output tensor by that letter filter
+                output *= self.letter_filters[idx]
+        return output
 
 
 class WordleModel(torch.nn.Module):
-    def __init__(self, num_words: int, hidden_multiplier: int = 26):
+    def __init__(self, words: List[Tuple[int, int, int, int, int]], hidden_multiplier: int = 26):
         super(WordleModel, self).__init__()
-        self.num_words = num_words
+        self.num_words = len(words)
+        self.words = words
 
         # Input Layers
         self.valid_alphabet_letters = torch.nn.Linear(
@@ -20,44 +60,36 @@ class WordleModel(torch.nn.Module):
         self.hidden_layer_stack = torch.nn.Sequential(
             torch.nn.Linear(57, 57*hidden_multiplier),
             torch.nn.Dropout(),
-            torch.nn.Linear(57*hidden_multiplier, num_words),
+            torch.nn.Linear(57*hidden_multiplier, self.num_words),
         )
 
         # Output Softmax Layer
-        self.output = torch.nn.Softmax(dim=0)
+        self.softmax_output = torch.nn.Softmax(dim=0)
+
+        self.invalid_word_processor = InvalidWordProcessorLayer(words)
+
+    def reset_word_processor(self):
+        self.invalid_word_processor.reset()
+
+    def to(self, device):
+        super(WordleModel, self).to(device)
+        self.invalid_word_processor.to(device)
 
     def forward(self, x):
-        current_word_as_list = x[2].tolist()
-
         # Input Layers
-        x_valid_alphabet = self.valid_alphabet_letters(x[0])
-        x_invalid_alphabet = self.invalid_alphabet_letters(x[1])
-        x_current_word = self.current_word_state(x[2])
-        x_num_turns_left = self.num_turns_left_state(x[3])
+        y_valid_alphabet = self.valid_alphabet_letters(x[0])
+        y_invalid_alphabet = self.invalid_alphabet_letters(x[1])
+        y_current_word = self.current_word_state(x[2])
+        y_num_turns_left = self.num_turns_left_state(x[3])
 
         # Hidden Layers
         # TODO: Add back the turns left layer
-        x = torch.cat((x_valid_alphabet, x_invalid_alphabet,
-                      x_current_word), dim=0)
-        x = self.hidden_layer_stack(x)
+        y = torch.cat((y_valid_alphabet, y_invalid_alphabet,
+                      y_current_word), dim=0)
+        y = self.hidden_layer_stack(y)
 
         # Output Softmax Layer
-        x = self.output(x)
+        y = self.softmax_output(y)
+        y = self.invalid_word_processor(y, x[1])
 
-        x_before_process = x.clone()
-
-        # Post-Procesing. Eliminate words where it cannot possibly be that.
-        for word_index in range(self.num_words):
-            # At this point, the word should be an encoding.
-            word = WORDS[word_index]
-            for idx, letter in enumerate(word):
-                # If this letter is in the invalid_alphabet, mark it 0.
-                if x_invalid_alphabet[letter] == 1:
-                    x_before_process[word_index] = 0
-
-                # If we know the letter for this position, filter out all words
-                # without that position
-                if current_word_as_list[idx] != -1 and current_word_as_list[idx] != letter:
-                    x_before_process[word_index] = 0
-
-        return x_before_process
+        return y
